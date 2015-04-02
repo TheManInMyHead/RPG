90b0c292ad628619efe09d64ae4d69267a41823d
tony <tony@localhost.localdomain>
2015-04-01 20:15:43 -0500

    Made webpage size relative to browser

--- mapFuncs.js
+++ mapFuncs.js
@@ -1,8 +1,14 @@
-var loadMap = function(background, foreground, playerX, playerY){
-	document.getElementById('background').src = background;
-	document.getElementById('foreground').src = foreground;
-	var playerFinalX = (playerX * 32) + 76;
-	var playerFinalY = (playerY * 32) + 568;
-	$("#player").css("top", playerFinalX);
-	$("#player").css("left", playerFinalY);
+var loadMap = function(background, backgroundWidth, backgroundHeight, foreground, foregroundWidth, foregroundHeight, playerX, playerY){
+	currentBack = document.getElementById('background');
+	currentBack.src = "Resources/"+background;
+	currentBack.width = backgroundWidth;
+	currentBack.height = backgroundHeight;
+	currentFore = document.getElementById('foreground');
+	currentFore.src = "Resources/"+foreground;
+	currentFore.width = foregroundWidth;
+	currentFore.height = foregroundHeight;
+	var playerFinalX = $(playerX + 22.65).toPx();
+	var playerFinalY = $(playerY + 6.6).toPx();
+	$("#player").css("left", playerFinalX);
+	$("#player").css("top", playerFinalY);
 }
